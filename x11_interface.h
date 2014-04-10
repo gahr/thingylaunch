b0898c360e63e9ce7f1881e6afffee9904dd3897
gahr <gahr>
2014-04-10 13:50:55 +0000

    Implement X11 with both libX11 and libxcb, select at build time

---
+++ x11_interface.h
@@ -0,0 +1,46 @@
+/*-
+ * Copyright (C) 2014 Pietro Cerutti <gahr@gahr.ch>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <string>
+
+typedef struct {
+    enum EventType {
+        Evt_Expose,
+        Evt_KeyPress
+    } type;
+    uint16_t key;
+    int state;
+} X11Event;
+
+struct X11Interface {
+    virtual ~X11Interface() { }
+    virtual bool createWindow(int width, int height) =0;
+    virtual bool setupGC(const std::string& bgColor, const std::string& fgColor, const std::string& fontDesc) =0;
+    virtual bool grabKeyboard() =0;
+    virtual bool redraw(const std::string& command, std::string::size_type cursorPos) =0;
+    virtual bool nextEvent(X11Event& ev) =0;
+
+    static X11Interface * create();
+};
